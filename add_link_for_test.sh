e52ace6f36b2d4aadb6b36c0fe15ba9e6f0e782e
root <root@team-ard.com>
2011-06-10 13:33:13 +0200

    modify add_link_for_test.sh

--- add_link_for_test.sh
+++ add_link_for_test.sh
@@ -2,3 +2,5 @@ ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/sites/default/settings.php ./sit
 ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/documents ./documents
 ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/files ./files
 ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/.htaccess ./.htaccess
+ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/images ./images
+ln -sf /opt/www/bruyninckxh2/web/www.orocos.org/sites/default/themes ./sites/default/themes
